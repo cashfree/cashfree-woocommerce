e69075eeef21c93787bef67f192286139b5b8523
rohan.gore <rohan.gore@cashfree.com>
2023-01-23 18:58:12 +0530

    New CDN widget link and app initialization structure updated

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,11 @@
-const merchantConfig = {
-  id: "cashfree-offer-widget", // id on which widget will load
-  token: "abc",
-};
+window.addEventListener("load", function () {
+  const widgetConfig = {
+    clientID: "1848d0ce8441fb8ffa258bc98481",
+  };
 
-// To initiate Widget
-const cashfree = CF_Offers(); // <merchantId>
+  // To initiate Widget
+  const cashfree = CF_Widget(widgetConfig); // <merchantId>
 
-// To call offersWidget by passing Merchant's Config
-cashfree.offersWidget(merchantConfig);
+  // To call offersWidget by passing Merchant's Config
+  cashfree.load();
+});
